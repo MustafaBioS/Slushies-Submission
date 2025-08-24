ad36fb5ceb676a5cfdf66673b81ab90364d420c4
MustafaBioS <bioshpere.voices@gmail.com>
2025-08-25 02:31:08 +0300

    added the actual task adding functionality and a delete button for them, currently working on an edit button so its a CRUD, i think...

--- models.py
+++ models.py
@@ -12,3 +12,13 @@ class User(db.Model, UserMixin):
 
     def __repr__(self):
         return f"<User {self.username}>"
+    
+class Task(db.Model):
+
+    __tablename__ = 'tasks'
+
+    id = db.Column(db.Integer, primary_key = True)
+    content = db.Column(db.String(64), nullable = False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+    user = db.relationship('User', backref=db.backref('tasks', lazy=True))
--- routes.py
+++ routes.py
@@ -1,7 +1,8 @@
 from flask import render_template, request, url_for, redirect, flash
-from flask_login import login_user
+from flask_login import login_required, login_user, current_user
 import sqlalchemy
 from models import User
+from models import Task
 
 def register_routes(app, db, bcrypt):
     @app.route('/', methods=['GET', 'POST'])
@@ -16,7 +17,7 @@ def register_routes(app, db, bcrypt):
 
             if user and bcrypt.check_password_hash(user.password, password):
                 login_user(user)
-                return "login succesful"
+                return redirect(url_for('tasks'))
 
             else:
                 flash("Incorrect Username or Password", "flash")
@@ -52,3 +53,40 @@ def register_routes(app, db, bcrypt):
             flash("Signup Failed", "flash")
             return redirect(url_for('signup'))
 
+    @app.route('/addtask', methods=['GET', 'POST'])
+    @login_required
+    def addtask():
+        if request.method == 'GET':
+            return render_template('add.html')
+        
+        if request.method == 'POST':
+
+            task_content = request.form.get('task')
+
+            new_task = Task(content=task_content, user_id=current_user.id)
+
+            db.session.add(new_task)
+            db.session.commit()
+
+            flash("Task Added Successfully", "flash")
+            return redirect(url_for('tasks'))
+        
+        else:
+            flash("Failed To Add Task", 'flash')
+            return redirect(url_for('tasks'))
+
+    @app.route('/tasks')
+    @login_required
+    def tasks():
+        tasks = Task.query.filter_by(user_id=current_user.id).all()
+        return render_template('index.html', tasks=tasks)
+    
+    @app.route('/delete/<int:task_id>', methods=['POST'])
+    @login_required
+    def delete_task(task_id):
+        task = Task.query.get_or_404(task_id)
+
+        db.session.delete(task)
+        db.session.commit()
+        flash("Task Deleted Successfully", 'flash')
+        return redirect(url_for('tasks'))
